a90d238ea5e91ad1c0de8b4b98b2180681cfc161
angel marquez <aglmarquez2904@gmail.com>
2025-06-16 11:55:50 -0400

    Delete static directory

--- static/ubicacione.js
+++
@@ -1,8 +0,0 @@
-export const parroquias = [
-    { parroquia: "Chacao" },
-    { parroquia: "Baruta" },
-    { parroquia: "El Hatillo" },
-    { parroquia: "La Pastora" },
-    { parroquia: "San Bernardino" }
-    // ...agrega las que quieras
-];
